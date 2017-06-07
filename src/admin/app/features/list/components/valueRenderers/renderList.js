ca082ac896042ee27a5ae2d41c1584cc5694039e
Josh Gretz <jgretz@truefit.io>
2017-06-07 12:40:46 -0400

    render 1:m relationships

--- src/admin/app/features/list/components/valueRenderers/renderList.js
+++ src/admin/app/features/list/components/valueRenderers/renderList.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {makeTitle} from '../../../shared/services';
 
-const render1to1 = (item, key, source) => (
-  <span>{item[key][source.display]}</span>
+const render1to1 = (item, source) => (
+  <span>{item[source.key][source.display]}</span>
 );
+
+const render1ToM = (item, itemKey, source) => {
+  const values = item[source.key] || [];
+
+  if (values.length > 0 && values.length <= 3) {
+    return values.map(x => x[source.display]).join(', ');
+  }
+
+  return (
+    <span>{values.length} {makeTitle(itemKey).toLowerCase()}</span>
+  );
+};
 
 const renderList = ({item, itemKey, schema}) => {
   const {source} = schema;
 
   if ((source.selections || 1) === 1) {
-    return render1to1(item, itemKey, source);
+    return render1to1(item, source);
   }
 
-  return <span>Longer List</span>;
+  return render1ToM(item, itemKey, source);
 };
 
 const RenderList = connect()(renderList);
