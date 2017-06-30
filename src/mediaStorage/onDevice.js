c58d138bbe274945b894ec2593f7512ed39c23d8
Josh Gretz <jgretz@truefit.io>
2017-06-29 21:08:03 -0400

    add option to compress images for on device

--- src/mediaStorage/onDevice.js
+++ src/mediaStorage/onDevice.js
@@ -1,20 +1,87 @@
 import fs from 'fs';
 import path from 'path';
 import {logError} from 'node-bits';
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
 
-export default config => ({
-  store: file => {
-    if (!fs.existsSync(config.path)) {
-      fs.mkdirSync(config.path);
+const DEFAULT_CONFIG = {
+  compressImages: false,
+  compressionQuality: '70-85',
+};
+
+class OnDevice {
+  constructor(config = {}) {
+    this.config = {
+      ...DEFAULT_CONFIG,
+      ...config,
+    };
+  }
+
+  // helper
+  tempPath() {
+    const {path: dirPath} = this.config;
+    return path.join(dirPath, 'temp');
+  }
+
+  ensureDirectories() {
+    const {path: dirPath, compressImages} = this.config;
+    const tempPath = this.tempPath();
+
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
     }
 
-    const filePath = path.join(config.path, file.name);
+    if (compressImages) {
+      if (!fs.existsSync(tempPath)) {
+        fs.mkdirSync(tempPath);
+      }
+    }
+  }
+
+  // STORE
+  storeNormal(file, filePath) {
     file.mv(filePath);
+  }
+
+  storeAndCompress(file, filePath) {
+    const {compressionQuality} = this.config;
+
+    file.mv(filePath, err => {
+      if (err) {
+        logError(err);
+        return;
+      }
+
+      imagemin([filePath], this.tempPath(), {
+        plugins: [
+          imageminJpegtran(),
+          imageminPngquant({quality: compressionQuality}),
+        ],
+      }).then(files => {
+        fs.unlinkSync(filePath);
+        fs.renameSync(files[0].path, filePath);
+      });
+    });
+  }
+
+  store(file) {
+    const {path: dirPath, compressImages} = this.config;
+    const filePath = path.join(dirPath, file.name);
+
+    this.ensureDirectories();
+
+    if (compressImages) {
+      this.storeAndCompress(file, filePath);
+    } else {
+      this.storeNormal(file, filePath);
+    }
 
     return filePath;
-  },
+  }
 
-  getImage: (req, res, db) => {
+  // GET
+  getImage(req, res, db) {
     const model = req.query.model;
     if (!model) {
       res.status(404).send('You must provide model for this configuration.');
@@ -47,5 +114,7 @@ export default config => ({
         logError(err);
         res.status(500).send(err);
       });
-  },
-});
+  }
+}
+
+export default config => new OnDevice(config);
